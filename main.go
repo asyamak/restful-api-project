94d93eaf732927b07e399b1ca3aef893bd3c6b91
Asya <Asya@MacBook-Aida.local>
2022-10-12 20:31:38 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+)
+
+func main() {
+	connS := "user=postgres dbname=postgrestest host=localhost port=5432 password=qwer1234"
+	db, err := sql.Open("postgres", connS)
+	if err != nil {
+		log.Printf("error open sql: %v", err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Printf("error in ping: %v", err)
+	}
+}
